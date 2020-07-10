55c53fb4455217bd6b9c46342d8604d45c84273e
Mason Davis <masondavis2009@gmail.com>
2020-07-10 12:24:01 -0500

    sprint

--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,27 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+axios.get("https://lambda-times-backend.herokuapp.com/topics")
+    .then(res => {
+        console.log(res.data.topics)
+
+        res.data.topics.forEach(topic => {
+            topics.appendChild(createTab(topic));
+        })
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
+const topics = document.querySelector(".topics")
+console.log(topics)
+
+function createTab(object) {
+    const tab = document.createElement("div");
+
+    tab.classList.add("tab");
+
+    tab.textContent = object;
+
+    return tab;
+}
